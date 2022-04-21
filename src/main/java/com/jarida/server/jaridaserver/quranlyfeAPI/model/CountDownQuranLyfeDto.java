7321aa1e5f5ab1d2d88483317f07a437b28e7926
Umar Saidu Auna <umarmanofpeace@gmail.com>
2022-04-21 16:45:35 +0100

    refactor: 💡 remove validator

--- src/main/java/com/jarida/server/jaridaserver/quranlyfeAPI/model/CountDownQuranLyfeDto.java
+++ src/main/java/com/jarida/server/jaridaserver/quranlyfeAPI/model/CountDownQuranLyfeDto.java
@@ -3,30 +3,20 @@ package com.jarida.server.jaridaserver.quranlyfeAPI.model;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Data
 public class CountDownQuranLyfeDto implements Serializable {
 
     @ApiModelProperty(value = "EndDateTime")
-    @NotEmpty(message = "End Date Time should not be null or empty")
-    @NotNull(message = "End Date Time should not be null or empty")
     private String endDateTime;
 
     @ApiModelProperty(value = "ShowCard")
-    @NotEmpty(message = "Show Card should not be null or empty")
-    @NotNull(message = "Show Card should not be null or empty")
     private Boolean showCard = false;
 
     @ApiModelProperty(value = "SuccessMessage")
-    @NotEmpty(message = "Success Message should not be null or empty")
-    @NotNull(message = "Success Message should not be null or empty")
     private String successMessage;
 
     @ApiModelProperty(value = "CountDownMessage")
-    @NotEmpty(message = "Count Down Message should not be null or empty")
-    @NotNull(message = "Count Down Message should not be null or empty")
     private String countDownMessage;
 }
